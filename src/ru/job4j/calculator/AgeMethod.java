ea05e011e0646f4f07dcfa0f88935ef8f37271e1
StepanZazyan <stepanzazyan@yandex.ru>
2021-06-29 14:44:19 +0300

    4.1. Аргументы [#295521 #172661]

---
+++ src/ru/job4j/calculator/AgeMethod.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculator;
+
+class ArgMethod {
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Stepan Zazyan";
+        int age = 30;
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+    }
+}
