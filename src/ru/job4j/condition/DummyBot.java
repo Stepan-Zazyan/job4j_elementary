a88f4460e6c96a08f08820f07bb50488355b728e
StepanZazyan <stepanzazyan@yandex.ru>
2021-07-03 12:24:53 +0300

    4.1.2. Глупый бот [#31323 #173823]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -1,17 +1,11 @@
 package ru.job4j.condition;
-//Если пришла фраза "Привет, Бот.", бот отвечает: "Привет, умник."
 
- //      Если пришла фраза "Пока.", бот отвечает: "До скорой встречи."
-
- //       Если пришла любая другая фраза,  бот отвечает: "Это ставит меня в тупик. Задайте другой вопрос."
 public class DummyBot {
-    @SuppressWarnings("checkstyle:RightCurly")
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник.";
-        }
-        if ("Пока.".equals(question)) {
+        } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
         }
         return rsl;
