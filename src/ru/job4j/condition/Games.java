ae19dc756c9a6de77cd6b198c6108567c50bcb23
StepanZazyan <stepanzazyan@yandex.ru>
2021-07-03 23:52:27 +0300

    4.3.1. Оператор && [#333783 #173900]

--- src/ru/job4j/condition/Games.java
+++ src/ru/job4j/condition/Games.java
@@ -5,9 +5,9 @@ public class Games {
     public static void permission(boolean allowByParent, boolean hasMoney) {
         if (allowByParent && hasMoney) {
             System.out.println("I can go to the cinema.");
-    } else {
+        } else {
         System.out.println("I can't.");
-    }
+        }
     }
 
     public static void main(String[] args) {
