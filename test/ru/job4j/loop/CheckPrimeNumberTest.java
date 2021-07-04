56c0751db9b35251f6cdc141a74dfb8674fa6e6d
StepanZazyan <stepanzazyan@yandex.ru>
2021-07-04 23:22:14 +0300

    5.5. Простое число [#171694]

---
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CheckPrimeNumberTest {
+
+    @Test
+    public void when181ThenTrue() {
+        int num = 181;
+        boolean result = CheckPrimeNumber.check(num);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void when1() {
+        int num = 1;
+        boolean result = CheckPrimeNumber.check(num);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void when2ThenFalse() {
+        int num = 4;
+        boolean result = CheckPrimeNumber.check(num);
+        Assert.assertFalse(result);
+    }
+}
