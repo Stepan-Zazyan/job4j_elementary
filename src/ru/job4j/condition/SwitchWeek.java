f4ac877f21f18292d21759bba6bc155e8ab8d12c
StepanZazyan <stepanzazyan@yandex.ru>
2021-07-04 15:31:02 +0300

    4.6.1. Switch [#334070]

---
+++ src/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+
+    public static String nameOfDay(int num) {
+        return switch (num) {
+            case 1 -> "Понедельник";
+            case 2 -> "Вторник";
+            case 3 -> "Среда";
+            case 4 -> "Четверг";
+            case 5 -> "Пятница";
+            case 6 -> "Суббота";
+            case 7 -> "Воскресенье";
+            default -> "Ошибка";
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(nameOfDay(1));
+    }
+}
