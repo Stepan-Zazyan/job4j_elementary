7f244d8b5eb542caadd7b021c6ec06e38d898e60
StepanZazyan <stepanzazyan@yandex.ru>
2021-07-04 15:55:40 +0300

    4.6.1. Switch [#334070]

---
+++ src/ru/job4j/condition/MultipleSwitchWeek.java
@@ -0,0 +1,46 @@
+package ru.job4j.condition;
+
+public class MultipleSwitchWeek {
+    public static int numberOfDay(String name) {
+        int i;
+        switch (name) {
+            case "Monday":
+            case "Понедельник":
+                i = 1;
+                break;
+            case "Tuesday":
+            case "Вторник":
+                i = 2;
+                break;
+            case "Wednesday":
+            case "Среда":
+                i = 3;
+                break;
+            case "Thursday":
+            case "Четверг":
+                i = 4;
+                break;
+            case "Friday":
+            case "Пятница":
+                i = 5;
+                break;
+            case "Sutirday":
+            case "Суббота":
+                i = 6;
+                break;
+            case "Sunday":
+            case "Воскресенье":
+                i = 7;
+                break;
+            default:
+                i = -1;
+                break;
+        }
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberOfDay("Monday"));
+    }
+}
--- src/ru/job4j/condition/SwitchWeek.java
+++ src/ru/job4j/condition/SwitchWeek.java
@@ -3,16 +3,34 @@ package ru.job4j.condition;
 public class SwitchWeek {
 
     public static String nameOfDay(int num) {
-        return switch (num) {
-            case 1 -> "Понедельник";
-            case 2 -> "Вторник";
-            case 3 -> "Среда";
-            case 4 -> "Четверг";
-            case 5 -> "Пятница";
-            case 6 -> "Суббота";
-            case 7 -> "Воскресенье";
-            default -> "Ошибка";
-        };
+        String day;
+        switch (num) {
+            case 1:
+                day = "Понедельник";
+                break;
+            case 2:
+                day = "Вторник";
+                break;
+            case 3:
+                day = "Среда";
+                break;
+            case 4:
+                day = "Четверг";
+                break;
+            case 5:
+                day = "Пятница";
+                break;
+            case 6:
+                day = "Суббота";
+                break;
+            case 7:
+                day = "Воскресенье";
+                break;
+            default:
+                day = "Ошибка";
+                break;
+        }
+        return day;
     }
 
     public static void main(String[] args) {
