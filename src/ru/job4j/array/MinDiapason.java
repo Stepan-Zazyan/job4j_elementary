65bfa90a7dd0361c165f9227eb8d12dacf286f13
StepanZazyan <stepanzazyan@yandex.ru>
2021-07-08 10:18:44 +0300

    6.5.3. Сортировка выборкой. [#156320 #175093]

--- src/ru/job4j/array/MinDiapason.java
+++ src/ru/job4j/array/MinDiapason.java
@@ -2,9 +2,9 @@ package ru.job4j.array;
 
 public class MinDiapason {
 
-    public static int findMin(int[] array, int start, int finish) {
+   public static int findMin(int[] array, int start, int finish) {
         int min = array[start];
-        for (int i = start; i <= finish; i++) {
+        for (int i = start; i < finish; i++) {
             if (array[i + 1] < array[i]) {
                 min = array[i + 1];
             }
--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -3,13 +3,10 @@ package ru.job4j.array;
 public class SortSelected {
 
     public static int[] sort(int[] data) {
-        for (int i = 0; i <= data.length / 2; i++) {
+        for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
             int index = FindLoop.indexOf(data, min, i, data.length - 1);
-            int[] swap = SwitchArray.swap(data, data[i], data[index]);
-            int temp = data[i];
-            data[i] = data[index];
-            data[index] = temp;
+            int[] swap = SwitchArray.swap(data, i, index);
         }
         return data;
     }
